1e679e2275901cbc9ecab60cd5914273a9deefaa
Cean Manzano <cemanzano@csumb.edu>
2018-08-27 22:29:05 +0000

    Made text as Header

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Cean Austin Manzano CST 336 site';
+echo '<h1>Cean Austin Manzano CST 336 site</h1>';
 
 ?>
 </body>
