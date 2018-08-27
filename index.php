012bac4738374c181d9a128a9a8f824dbb6c05dd
Cean Manzano <cemanzano@csumb.edu>
2018-08-27 22:19:42 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Cean Austin Manzano CST 336 site';
+
+?>
+</body>
+</html>
