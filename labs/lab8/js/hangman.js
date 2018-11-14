24cf155e6865d1e2a80f0f499414ca6c57d5d4ca
Cean Manzano <cemanzano@csumb.edu>
2018-11-14 18:32:59 +0000

    Homework 4

---
+++ labs/lab8/js/hangman.js
@@ -0,0 +1,82 @@
+var selectedWord = "";
+var selectedHint = "";
+var board = [];
+var remainingGuesses = 6;
+var words = ["snake", "monkey", "beetle"];
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// listener
+window.onload = startGame();
+
+
+// functions
+function startGame() {
+    pickWord();
+    initBoard();
+    updateBoard();
+    createLetters();
+}
+
+function initBoard() {
+    for (var letter in selectedWord) {
+        board.push("_");
+    }
+}
+
+function pickWord() {
+    var randomInt = Math.floor(Math.random() * words.length);
+    selectedWord = words[randomInt].toUpperCase();
+}
+
+
+function updateBoard() {
+    for (var letter of board) {
+        document.getElementById("word").innerHTML += letter + " ";
+    }
+}
+
+
+
+function createLetters() {
+    for (var letter of alphabet) {
+        $("#letters").append("<button class='letter' id='" + letter + "'>" + letter + "</button>");
+    }
+}
+
+function checkLetter(letter) {
+    var positions = new Array();
+    
+    for (var i = 0; i < selectedWord.length; i++) {
+        console.log(selectedWord);
+        if (letter == selectedWord[i]) {
+            positions.push([i]);
+        }
+    }
+    
+    if (positions.length > 0) {
+        updateWord(positions, letter);
+    }
+    else {
+        remainingGuesses -= 1;
+    }
+}
+
+function updateWord(positions, letter) {
+    for (var pos of positions) {
+        board[pos] = letter;
+    }
+    updateBoard();
+}
+
+$(".letter").click(function() {
+    console.log($(this).attr("id"));
+});
+
+
+
+// $("#letterBtn").click(function() {
+//     var boxVal = $('$letterBox').val();
+//     console.log("You pressed the button and it had the value: " + boxVal);
+// })
