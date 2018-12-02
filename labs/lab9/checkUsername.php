0463b47a589d97003fca854f8390ec017923d2d8
Cean Manzano <cemanzano@csumb.edu>
2018-12-02 07:46:41 +0000

    Testing

--- labs/lab9/checkUsername.php
+++ labs/lab9/checkUsername.php
@@ -1,8 +1,20 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-include '../../inc/dbConnection.php';
-$dbConn = startConnection("ottermart");
 
+function startConnection() {
+    // Creating connection
+    $host = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "b831dbdd87260c";
+    $password = "d170c72e";
+    $dbname = "heroku_c149aff39c41e5d";
+    
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $dbConn;
+}
+
+$dbConn = startConnection();
 
 $sql = "SELECT * FROM om_admin WHERE username =:username ";
 
